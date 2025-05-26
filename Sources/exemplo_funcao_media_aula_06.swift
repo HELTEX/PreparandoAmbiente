6f96c22eff4ad0fedfd5e61f5ec68625f05f5ffa
HELTEX <10742524@mackenzista.com.br>
2025-05-26 16:04:05 -0300

    Update exemplo_funcao_media_aula_06.swift

--- Sources/exemplo_funcao_media_aula_06.swift
+++ Sources/exemplo_funcao_media_aula_06.swift
@@ -1,6 +1,6 @@
-//Função que retorna média 
-func media(numero1: Int, numero2: Int) -> Int {
-    return (numero1 + numero2) / 2
+//exemplo função
+func media(numero1: Int, numero2: Int) -> Int { //função média, nome média(de um número inteiro, de outro número inteiro) que irá retornar um numero inteiro
+    return (numero1 + numero2) / 2              //retorna a média entre dois números 
 }
 
 print("Digite o primeiro número: ")
@@ -8,5 +8,5 @@ let valor1 = Int(readLine()!)!
 print("Digite o segundo número: ")
 let valor2 = Int(readLine()!)!
 
-let resultado = media(numero1: valor1, numero2: valor2)
+let resultado = media(numero1: valor1, numero2: valor2) //cria uma variável que vai chamar a função média, que vai calcular os números lidos
 print("A média é \(resultado)")
